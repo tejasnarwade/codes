43bd3259d6a6fc6c573b8204ea9596de9c7c7cc0
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-02-25 21:51:00 +0530

    Time: 4 ms (47.09%) | Memory: 20.5 MB (62.75%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        maxnum : int = len(nums)
-        idealsum =0
-        for i in range(0,maxnum+1):
-            idealsum+=i
+        n : int = len(nums)
+        idealsum = n*(n+1)//2
+        #for i in range(0,maxnum+1):
+            #idealsum+=i
 
         rlsum = sum(nums)
         return idealsum - rlsum
