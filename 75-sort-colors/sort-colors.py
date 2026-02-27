80212aa0f07b75dfe642d77d1583418e03f55b6c
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-02-28 00:59:14 +0530

    Time: 3 ms (11.36%) | Memory: 19.1 MB (86.37%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,7 +3,7 @@ class Solution:
         #using bubble sort
         n = len(nums)
         for i in range(n):
-            for j in range(n-1):
+            for j in range(n-i-1):
                 if nums[j]>nums[j+1]:
                     nums[j],nums[j+1]=nums[j+1],nums[j]
         
