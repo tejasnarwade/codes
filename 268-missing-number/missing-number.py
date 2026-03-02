eaaeed627544a0ab384c0ad4b62797b6c3cd2c39
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-03-03 00:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (89.92%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,10 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n : int = len(nums)
-        idealsum = n*(n+1)//2
-        #for i in range(0,maxnum+1):
-            #idealsum+=i
-
-        rlsum = sum(nums)
-        return idealsum - rlsum
+       return (len(nums)*(len(nums)+1)//2)-sum(nums)
         
+        #formula: n*(n+1)/2---> gives us total sum of arrray of the number wasnt missing , so we will do idealsum-actual sum = missingnumber
