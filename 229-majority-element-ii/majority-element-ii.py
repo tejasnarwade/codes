305fa5ba1871c5e115af342b8450118ba610bfb1
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-03-09 19:50:47 +0530

    Time: 7 ms (67.60%) | Memory: 23.5 MB (37.59%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,16 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        target = n/3
-        result=[]
+        #bruteforce
+        #n = len(nums)
+        #target = n/3
+        #result=[]
         '''     brute force where i check frequency of each via nums.count(element)>target
         for x in nums:
             if nums.count(x)>target and x not in result:
                 result.append(x)
         return result
         '''
+
         #optimal approach
         freqdict = {}
         for x in nums:
             freqdict[x]=freqdict.get(x,0)+1
-        return [x for x,y in freqdict.items() if y>target]
+        return [x for x,y in freqdict.items() if y>len(nums)//3]
