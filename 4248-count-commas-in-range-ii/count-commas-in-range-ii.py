c01712b4a641a5619fcc2c5c957736de4d9824ca
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-03-16 23:57:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (100.00%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.py
@@ -1,8 +1,14 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-        cnt = 0
-        p = 1000
-        while p <= n:
+        cnt = 0      # ithe total comma count initialize kela
+        p = 1000     # ithe pahila comma 1000 pasun suru hoto
+
+        while p <= n:   # jo paryant p n peksha motha nahi to paryant loop chalel
+            
+            # p pasun n paryant je numbers aahet tyat ek comma contribute hoil
             cnt += n - p + 1
+            
+            # pudhcha comma level (1000 -> 1000000 -> 1000000000 ...)
             p *= 1000
-        return cnt
+
+        return cnt   # final comma count return karto
