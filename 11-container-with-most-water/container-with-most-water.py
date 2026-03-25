f9cc6885f68131af71f14b79f0b9364796abb45f
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-03-25 10:36:25 +0530

    Time: 68 ms (8.59%) | Memory: 29.5 MB (79.80%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,48 @@
+class Solution:
+
+    #we will be using two pointer pattern as we need to find pairs with maximum sum where left one being greater
+    def maxArea(self, height: List[int]) -> int:
+        l=0   # we will start at 0 the left pointer
+        r=len(height)-1      #right ponter will start from end index
+        max_water=0
+        while l<r:  
+            # until right index not equal to left index
+            h = min(height[l],height[r]) #height will be minimum one between height[l] and height[r]
+            w = r - l     #distance between 2 pointers
+            area = h * w
+            max_water = max(max_water,area)
+            # jr left value lesser ahe tr left as comapred to right such that we have to acccumulate maximum amount of water so need maximum height[left]
+            if height[l]<height[r]:
+                l=l+1
+            else:
+                r=r-1
+        return max_water
+        
+
+
+        #author:tejasnarwade
+        '''
+for my reference here is the template for array two pointer:
+
+    def two_pointer(arr):
+    l=0 
+    r = len(arr) - 1
+    ans = 0   # or inf / [] depending on problem
+    
+    while l < r:
+        # 1. compute result
+        val = min(arr[l], arr[r]) * (r - l)   # change logic per problem
+        ans = max(ans, val)
+        
+        # 2. move pointer (KEY PART)
+        if arr[l] < arr[r]:
+            l += 1
+        else:
+            r -= 1
+    
+    return ans
+
+
+
+        '''
+
