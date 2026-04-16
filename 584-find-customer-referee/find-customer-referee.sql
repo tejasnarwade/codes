259ba2f3ad6747c5142f860a2f0454f84ff003b6
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-04-16 08:00:43 +0530

    Time: 451 ms (69.58%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,3 +1,1 @@
-SELECT name from Customer WHERE referee_id!=2 or referee_id is null;
-
-#we can use "is" to check if field = null
+SELECT name from Customer where referee_id <>2 or referee_id is null;
