73f237fb8d8b30caf5254a19df31e11f86e8f103
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-04-16 08:07:55 +0530

    Time: 278 ms (69.39%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,1 +1,4 @@
-SELECT name,population,area FROM World WHERE area>=3000000 or population>=25000000;
+SELECT w.name,w.population,w.area from World w where w.area>=3000000 or w.population>=25000000 ORDER BY w.area;
+
+# here order by and making w as alias of World; you can simply write it as :
+# SELECT name,population,area from World where area >=3000000 or population>=25000000;
