83590eabc9bd23023d5962d88793233a1f53160d
Tejas Narwade <tejasnarwade.eng@gmail.com>
2026-04-16 08:08:51 +0530

    Time: 284 ms (56.68%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,4 @@
-SELECT w.name,w.population,w.area from World w where w.area>=3000000 or w.population>=25000000 ORDER BY w.area;
+#SELECT w.name,w.population,w.area from World w where w.area>=3000000 or w.population>=25000000 ORDER BY w.area;
 
-# here order by and making w as alias of World; you can simply write it as :
-# SELECT name,population,area from World where area >=3000000 or population>=25000000;
+#here order by and making w as alias of World; you can simply write it as :
+ SELECT name,population,area from World where area >=3000000 or population>=25000000;
